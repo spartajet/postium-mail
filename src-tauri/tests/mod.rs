f8d5d708f466af3777d3ebc95a2b2c9e16a75ae9
spartajet <guoxiaozhong1990@outlook.com>
2026-03-17 14:20:26 +0800

    Reorganize test module imports alphabetically

--- src-tauri/tests/mod.rs
+++ src-tauri/tests/mod.rs
@@ -1,9 +1,8 @@
 mod smtp_integrate_test;
-mod test_sync_since;
-mod test_163_full_emails;
 mod test_163_folder_names;
-mod test_garbled_subject;
-mod test_uid_2408;
-mod test_raw_email;
+mod test_163_full_emails;
 mod test_decode_subject;
-mod test_attachments;
+mod test_garbled_subject;
+mod test_raw_email;
+mod test_sync_since;
+mod test_uid_2408;
